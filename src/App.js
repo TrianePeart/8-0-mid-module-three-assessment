3254c254bc3a75ca663d758d1cc1cc4b4da72b82
Triane Peart <TrianePeart@gmail.com>
2021-11-21 15:10:03 -0500

    Save Point: I might be overthinking this

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import "./App.css";
+import React from "react";
+import cart from "./components/cart"; 
+import data from "./data/productData";
+import product from "./components/product";
+import products from "./components/products";
+import Checkout from  "./components/Checkout";
 
 const App = () => {
   return <h1>My Garage Sale</h1>;
---
+++ src/components/product.js
@@ -0,0 +1,3 @@
+import formatPrice from "../helpers/formatPrice";
+
+const product = ({name, img, descripton})
